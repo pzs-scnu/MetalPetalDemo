8e24991a0ea1f033c0b9870dc5ddc27cd998e733
panzhansheng <panzs@m.scnu.edu.cn>
2024-11-28 21:46:40 +0800

    Initial Commit

---
+++ MetalPetalDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalPetalDemo
+//
+//  Created by zhansheng pan on 2024/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalPetalDemo/MetalPetalDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalPetalDemoApp.swift
+//  MetalPetalDemo
+//
+//  Created by zhansheng pan on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct MetalPetalDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
